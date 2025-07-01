73c7869e700b132e9b1772cb4e5efc0ea9843bc2
Mateusz Mańczak <mateuszmanczak@icloud.com>
2025-07-01 14:10:08 +0200

    Paint based on time, direction and speed

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-const core = document.querySelector('#core');
-const width = core.clientWidth;
-const height = core.clientHeight;
-
 Number.prototype.toPixel = function () {
 	return `${this}px`;
 };
@@ -10,26 +6,50 @@ Number.prototype.divide = function (divisor) {
 	return this / divisor;
 };
 
-const barHeight = (time, waveLength, amplitude) => {
-	return (Math.sin((time * (Math.PI * 2)) / waveLength) * amplitude * 2) / 2 + amplitude;
+const barHeight = (x, waveLength, amplitude) => {
+	return (Math.sin((x * (Math.PI * 2)) / waveLength) * amplitude * 2) / 2 + amplitude;
 };
 
+const paint = (container, waves, time) => {
+	for (i = 0; i < width; i++) {
+		const bar = document.createElement('div');
+		bar.classList.add('bar');
+
+		bar.style.height = waves
+			.reduce(
+				(acc, wave) =>
+					acc +
+					barHeight(
+						i + wave.direction * wave.speed * time,
+						wave.waveLength,
+						wave.amplitude,
+					),
+				0,
+			)
+			.divide(waves.length)
+			.toPixel();
+
+		bar.style.left = i.toPixel();
+		container.append(bar);
+	}
+};
+
+const core = document.querySelector('#core');
+const width = core.clientWidth;
+const height = core.clientHeight;
+
+// Direction: 1=right, -1=left
 const waves = [
-	{ waveLength: width / 4, amplitude: height / 2 },
-	{ waveLength: width / 8, amplitude: height / 3 },
-	{ waveLength: width / 5, amplitude: height / 2.5 },
-	{ waveLength: width / 2, amplitude: height / 8 },
+	{ waveLength: width / 4, amplitude: height / 2, direction: 1, speed: 10 },
+	{ waveLength: width / 8, amplitude: height / 3, direction: 1, speed: 20 },
+	{ waveLength: width / 5, amplitude: height / 2.5, direction: -1, speed: 15 },
+	{ waveLength: width / 2, amplitude: height / 8, direction: 1, speed: 13 },
 ];
 
-for (i = 0; i < width; i++) {
-	const bar = document.createElement('div');
-	bar.classList.add('bar');
-
-	bar.style.height = waves
-		.reduce((acc, wave) => acc + barHeight(i, wave.waveLength, wave.amplitude), 0)
-		.divide(waves.length)
-		.toPixel();
-
-	bar.style.left = i.toPixel();
-	core.append(bar);
-}
+paint(core, waves);
+let time = 0;
+setInterval(() => {
+	core.innerHTML = '';
+	time += 0.1;
+	paint(core, waves, time);
+}, 100);
